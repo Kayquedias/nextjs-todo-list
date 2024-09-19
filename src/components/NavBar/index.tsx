77179e4ed8fb5391e715ba1a89f3063d881a87c2
Kayquedias <kayquedias204@gmail.com>
2024-09-19 18:52:37 -0300

    lint: NavBar component

--- src/components/NavBar/index.tsx
+++ src/components/NavBar/index.tsx
@@ -1,4 +1,4 @@
-import Image from "next/image";
+import Image from 'next/image'
 
 import LogoImage from '@/assets/logo-image.png'
 import LogoText from '@/assets/logo-text.svg'
@@ -9,21 +9,11 @@ export function NavBar() {
   return (
     <header className={styles.heading}>
       <div className={styles.logoWrapper}>
-        <Image 
-          src={LogoImage}
-          alt=""
-          width={32}
-          height={32}
-        />
-        <Image 
-          src={LogoText}
-          alt=""
-        />
+        <Image src={LogoImage} alt="" width={32} height={32} />
+        <Image src={LogoText} alt="" />
       </div>
 
-      <h1 className={styles.title}>
-        Bem vindo de volta, Marcus
-      </h1>
+      <h1 className={styles.title}>Bem vindo de volta, Marcus</h1>
 
       <span className={styles.dateInfo}>Segunda, 01 de dezembro de 2025</span>
     </header>
