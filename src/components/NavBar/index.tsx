c2b2def89576a844158357dc3a9aa4026344badd
Kayquedias <kayquedias204@gmail.com>
2024-09-19 21:24:33 -0300

    revert: last commit changes

--- src/components/NavBar/index.tsx
+++ src/components/NavBar/index.tsx
@@ -3,13 +3,12 @@ import Image from 'next/image'
 import styles from './styles.module.scss'
 
 import LogoText from '@/components/assets/logo-text.svg'
-import LogoImage from '@/components/assets/logo-image.png'
 
 export function NavBar() {
   return (
     <header className={styles.heading}>
       <div className={styles.logoWrapper}>
-        <Image src={LogoImage} alt="" width={32} height={32} />
+        <Image src={'/assets/logo-image.png'} alt="" width={32} height={32} />
         <Image src={LogoText} alt="" />
       </div>
 
